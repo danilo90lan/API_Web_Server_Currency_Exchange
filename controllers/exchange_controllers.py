b08022f30ad22f66ba477744fe314a0f49c9e242
danilo90lan <15630@coderacademy.edu.au>
2024-09-16 20:07:00 +1000

    used check_account_user() function in the exchange controller

--- controllers/exchange_controllers.py
+++ controllers/exchange_controllers.py
@@ -10,11 +10,6 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 exchange_bp = Blueprint("exchange", __name__, url_prefix="/<int:account_id>")
 
-# @exchange_bp.route("/")
-# def get_all_exchanges():
-#     statement = db.select(Exchange).order_by(Exchange.date_time.asc())
-#     exchanges = db.session.scalars(statement)
-#     return jsonify(exchanges_schema.dump(exchanges))
 
 @exchange_bp.route("/exchange-history")
 @jwt_required()
@@ -41,65 +36,59 @@ def get_exchanges(account_id):
 @exchange_bp.route("/transfer/<int:destination_id>", methods=["POST"])
 @jwt_required()
 def currency_exchange(account_id, destination_id):
-    body = request.get_json()
-
-    if account_id != destination_id:
-        statement = db.select(Account).filter_by(account_id=account_id)
-        account_from = db.session.scalar(statement)
-        if account_from:
-            statement = db.select(Account).filter_by(account_id=destination_id)
-            account_to = db.session.scalar(statement)
-            if not account_to:
-                return {"error":"destination account does NOT exist!"}
-        else:
-            return {"error":"origin account does NOT exist!"}
-    else:
+    if account_id == destination_id:
         return {"error":"Cannot transfer funds to the same account. Please select a different account."}
     
-    # check if both accounts belong to the current user
-    current_user = int(get_jwt_identity())
-    if account_from.user_id == current_user and account_to.user_id == current_user:
-       
-       # check 
-
-        if body.get("amount") > 0:
-            if account_from.balance >= body.get("amount"):
-                account_from.balance -= body.get("amount")
+    body = request.get_json()
+    verify_account_origin = check_account_user(account_id)
+    verify_account_destination = check_account_user(destination_id)
+    # verify if both accounts belong to the current user
+    if verify_account_origin==True: 
+        if verify_account_destination==True:
+            # get the amount to transfer and verify if it's a positive value
+            amount = body.get("amount")
+            if amount > 0:
+                statement = db.select(Account).filter_by(account_id=account_id)
+                account_from = db.session.scalar(statement)
+                if account_from.balance >= amount:
+                    account_from.balance -= amount
+                else:
+                    return {"error": f"Insufficient funds in the account {account_from.account_id}."}
             else:
-                return {"error": f"Insufficient funds in the account {account_from.account_id}."}
-        else:
-            return {"error":"Amount MUST be greater than 0"}
-        
-        # get the amount to transfer
-        amount = body.get("amount")
-
-        # check if the two accounts have different currency_codes
-        # if different currency_code need the conversion
-        if account_from.currency_code != account_to.currency_code:
-            statement = db.select(Currency).filter_by(currency_code=account_from.currency_code)
-            currency_from = db.session.scalar(statement)
-
-            statement = db.select(Currency).filter_by(currency_code=account_to.currency_code)
-            currency_to = db.session.scalar(statement)
-            # convertimg the rates from Currency A to currency B
-            amount_exchanged = (amount / currency_from.rate) * currency_to.rate
-        else:
-            amount_exchanged = amount
-
-        account_to.balance = float(account_to.balance) + amount_exchanged
-
-        # create a new instance of Exchange
-        new_exchange = Exchange(
-            amount = body.get("amount"),
-            amount_exchanged = amount_exchanged,
-            description = body.get("description"),
-            account_origin = account_from,
-            account_destination = account_to
-        )
-
-        db.session.add(new_exchange)
-        db.session.commit()
-        return jsonify(exchange_schema.dump(new_exchange))
+                return {"error":"Amount MUST be greater than 0"}
+        else: return verify_account_destination
     else:
-        return {"error":f"The account/s DO NOT belong to the user {current_user}"}
+        return verify_account_origin
         
+
+    # check if the two accounts have different currency_codes
+    # if different currency_code need the conversion
+    statement = db.select(Account).filter_by(account_id=destination_id)
+    account_to = db.session.scalar(statement)
+    if account_from.currency_code != account_to.currency_code:
+        statement = db.select(Currency).filter_by(currency_code=account_from.currency_code)
+        currency_from = db.session.scalar(statement)
+
+        statement = db.select(Currency).filter_by(currency_code=account_to.currency_code)
+        currency_to = db.session.scalar(statement)
+        # convertimg the rates from Currency A to currency B
+        amount_exchanged = (amount / currency_from.rate) * currency_to.rate
+    else:
+        amount_exchanged = amount
+
+    # update the balance of the destination accont
+    account_to.balance = float(account_to.balance) + amount_exchanged
+
+    # create a new instance of Exchange
+    new_exchange = Exchange(
+        amount = body.get("amount"),
+        amount_exchanged = amount_exchanged,
+        description = body.get("description"),
+        account_origin = account_from,
+        account_destination = account_to
+    )
+
+    db.session.add(new_exchange)
+    db.session.commit()
+    return jsonify(exchange_schema.dump(new_exchange))
+    
--- utils/authorization.py
+++ utils/authorization.py
@@ -18,12 +18,12 @@ def check_account_user(account_id):
     statement = db.select(Account).filter_by(account_id=account_id)
     account = db.session.scalar(statement)
     if not account:
-        return {"error":"The account does NOT exist!"}, 404
+        return {"error":f"The account ID {account_id} does NOT exist!"}, 404
 
     # Get the user_id from JWT identity
     user_id = int(get_jwt_identity())
     # Check if the account belongs to the user
     if account.user_id != user_id:
-        return {"error": f"The account {account_id} doesn NOT belong to the current user"}
+        return {"error": f"The account ID {account_id} doesn NOT belong to the current user!"}
     else:
         return True
