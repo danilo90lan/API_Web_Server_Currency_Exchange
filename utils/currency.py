1fcc6ed33103715403932b05ef4a645814981c6c
danilo90lan <15630@coderacademy.edu.au>
2024-09-20 21:51:04 +1000

    currency.py error  handling

--- utils/currency.py
+++ utils/currency.py
@@ -1,16 +1,28 @@
 from init import db
 import requests
+from requests.exceptions import HTTPError, Timeout, RequestException
+
 from models.currency import Currency
 from sqlalchemy import func
 from sqlalchemy.exc import SQLAlchemyError
 
 def get_currencies():
     endpoint = "https://v6.exchangerate-api.com/v6/b12835ecd29b6518d756378d/latest/USD"
-    response = requests.get(endpoint)
-    if response.ok:
+    try:
+        response = requests.get(endpoint)
+        # raise an exception if a 4XX or 5XX error occurs
+        response.raise_for_status()
         return response.json()
-    else:
-        return "The request failed", response.status_code
+
+    except HTTPError as http_err:
+        return f"HTTP error occurred: {http_err}", 500
+    except Timeout:
+        return "The request timed out", 408         # 408 is the HTTP status code for Request Timeout
+    except RequestException as req_err:
+        return f"Request failed: {req_err}", 500
+    except Exception as e:
+        db.session.rollback()
+        return {"error": f"Unexpected error occurred: {str(e)}"}, 500
     
 def seed_currency_table():
     list_currency = []
@@ -32,29 +44,33 @@ def seed_currency_table():
     except SQLAlchemyError as e:
         db.session.rollback()
         return {"error": f"Database operation failed {e}"}, 500
+    except Exception as e:
+        db.session.rollback()
+        return {"error": f"Unexpected error occurred: {str(e)}"}, 500
 
 
 def update_exchange_rates(app):
     with app.app_context():
         currency = get_currencies()
-        # Loop through the currency data and update the corresponding rows
-        for code, rate in currency["conversion_rates"].items():
-            # Update only the existing records
-            db.session.query(Currency).filter_by(currency_code=code).update({
-                "rate": rate,
-                "last_update": func.now()
-            })
         try:
+            # Loop through the currency data and update the corresponding rows
+            for code, rate in currency["conversion_rates"].items():
+                # Update only the existing records
+                db.session.query(Currency).filter_by(currency_code=code).update({
+                    "rate": rate,
+                    "last_update": func.now()
+                })
             db.session.commit()
             print("Currencies updated successfully!")
         except SQLAlchemyError as e:
             db.session.rollback()
             return {"error": f"Database operation failed {e}"}, 500
+        except Exception as e:
+            db.session.rollback()
+            return {"error": f"An unexpected error occurred: {str(e)}"}, 500
+
 
 def get_currencies_codes():
     currency = get_currencies()
-    list_currency_codes = []
-    for i in currency["conversion_rates"].items():
-        code = i[0]
-        list_currency_codes.append(code)
+    list_currency_codes = currency["conversion_rates"].keys()
     return tuple(list_currency_codes)
